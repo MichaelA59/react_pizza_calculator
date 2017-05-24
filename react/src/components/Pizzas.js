692f05b65d78bd28ac9fcecab5e4865f028f5abd
Michael Adamski <mvadamski@gmail.com>
2017-05-23 22:48:42 -0400

    i cant stop adding gifs

--- react/src/components/Pizzas.js
+++ react/src/components/Pizzas.js
@@ -20,30 +20,38 @@ class Pizzas extends React.Component {
 
   render() {
     let amountPizza;
+    let statefulbackground;
     if (this.state.pizzas > 1000) {
+      statefulbackground = 'gifback';
       amountPizza = <img src='http://www.reactiongifs.com/wp-content/uploads/2013/12/please.gif'></img>
     }
     else if (this.state.pizzas > 500) {
+      statefulbackground = '';
       amountPizza = <img src='https://media.giphy.com/media/l4Ki2obCyAQS5WhFe/giphy.gif'></img>
     }
     else if (this.state.pizzas > 400) {
+      statefulbackground = '';
       amountPizza = 'My Suggestion? Salad man, lots of it!';
     }
     else if (this.state.pizzas > 150) {
+      statefulbackground = '';
       amountPizza = 'Ok, actually you need help...';
     }
     else if (this.state.pizzas > 50) {
+      statefulbackground = '';
       amountPizza = 'To open your own pizza shoppe!';
     }
     else if (this.state.pizzas == null) {
+      statefulbackground = '';
       amountPizza = '';
     }
     else {
+      statefulbackground = '';
       amountPizza = `${this.state.pizzas} pizzas`
     }
 
     return(
-      <div>
+      <div className={statefulbackground}>
       <form>
         <input type="text" placeholder="Number of People" value={this.state.people} onChange={this.handleChange} ></input>
       </form>

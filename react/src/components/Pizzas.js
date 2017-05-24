a14d10f36de785d12728cc2c14c8d8bcf850ce7d
Michael Adamski <mvadamski@gmail.com>
2017-05-23 22:17:32 -0400

    modified text print out based on numbers

--- react/src/components/Pizzas.js
+++ react/src/components/Pizzas.js
@@ -19,12 +19,21 @@ class Pizzas extends React.Component {
   }
 
   render() {
+    let amountPizza;
+    if (this.state.pizzas > 50) {
+      amountPizza = 'To open your own pizza shoppe!';
+    }
+    else {
+      amountPizza = `${this.state.pizzas} pizzas`
+    }
+    
     return(
       <div>
       <form>
         <input type="text" placeholder="Number of People" value={this.state.people} onChange={this.handleChange} ></input>
       </form>
-      <h2>You need {this.state.pizzas} pizzas</h2>
+      <h2>You need </h2>
+      <h2>{amountPizza}</h2>
       </div>
     )
   }
